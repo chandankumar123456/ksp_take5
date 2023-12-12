7547926aa9055a94a80899ee6b030065f7a6301b
Vanjarapu Chandan Kumar <43309474+chandankumar123456@users.noreply.github.com>
2023-12-12 07:41:59 +0530

    Added Files Via Upload

---
+++ test.js
@@ -0,0 +1,25 @@
+console.log("Kick Start Program Day - 5");
+console.log("Hello World!");
+//variables
+let c = 23;
+let b = 29;
+//operators
+let ans;
+ans = c + b;
+console.log("Addition is " + ans);
+ans = ++c + --b;
+console.log("Increment and decrement ans : " + ans); //ans = 24+28 = 52
+ans = c ** b;
+console.log(`Power : ${c} ^ ${b} = ${ans}`);
+let a = 23;
+let e = '23';
+console.log(`a = ${a} and e = '${e}'`);
+//Equal to operator( == )
+console.log("Equal to operator( == ) : ");
+console.log(`a == e(${a == e})`);
+//Strictly Equal to operator( === )
+console.log("Strictly equal to operator( === ) : ");
+console.log(`a === e(${a === e})`);
+//Not Equal to operator( != )   
+console.log("Not equal to operator( != ) : ");
+console.log(`a !== e(${a !== e})`);
